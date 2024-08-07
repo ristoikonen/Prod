1ed65bb793e43ac70fd6bcbd1cb269e5b21a69c1
Risto Ikonen <ristoikonen@gmail.com>
2024-08-07 14:18:18 +1000

    work with html

---
+++ se.js
@@ -0,0 +1,33 @@
+fetch('https://api.github.com/users/username')
+    .then(response => response.json())
+    .then(data => {
+        // Process the data here
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle any errors here
+        console.error(error);
+    });
+    fetch('https://api.github.com/search/code?q=YOUR_QUERY')
+        .then(response => response.json())
+        .then(data => {
+            // Process the search results here
+            console.log(data.items);
+        })
+        .catch(error => {
+            // Handle any errors here
+            console.error(error);
+        });
+
+        function searchCode(queryString) {
+            fetch(`https://api.github.com/search/code?q=${queryString}`)
+                .then(response => response.json())
+                .then(data => {
+                    // Process the search results here
+                    console.log(data.items);
+                })
+                .catch(error => {
+                    // Handle any errors here
+                    console.error(error);
+                });
+        }
